836ae2cdc3ca2f21881bfe6fdc0a4363ff598ea5
Chris Marotta <ctm5vjg@virginia.edu>
2024-04-29 12:38:11 -0400

    chris skill issue

--- cmd/skeld/main.go
+++ cmd/skeld/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"os"
 
 	"github.com/TheRealChrisM/SUSC2/pkg/skserver"
@@ -11,14 +10,11 @@ import (
 func main() {
 
 	cmds := make(chan string)
-	cmds <- "echo hello"
-	fmt.Print(cmds)
 	skserver.Serve(&cmds)
 
 	var s = bufio.NewScanner(os.Stdin)
 
 	for s.Scan() {
 		cmds <- s.Text()
-		fmt.Print(cmds)
 	}
 }
